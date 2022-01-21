42a1eccbab631416875ced9eddb82c5487f41642
Candy Fair <candice.bastien@gmail.com>
2022-01-21 19:43:18 +0100

    Add MUI style to NavBar

--- FRONT/src/components/NavBar/index.js
+++ FRONT/src/components/NavBar/index.js
@@ -1,13 +1,29 @@
+// MaterialUI imports
+import { makeStyles } from '@mui/styles';
+import { Box } from '@mui/material/';
 // === Imports
 import SearchBar from './SearchBar';
 import Login from './LoginButtons';
 
-const NavBar = () => (
-  <div className="navbar">
-    <div>Logo O'Rent</div>
-    <SearchBar />
-    <Login />
-  </div>
-);
+const useStyles = makeStyles((theme) => ({
+  navbar: {
+    display: 'flex',
+  },
+}));
 
+const NavBar = () => {
+  const classes = useStyles();
+  return (
+    <Box
+      sx={{ p: 2 }}
+      flexDirection="row"
+      justifyContent="space-between"
+      className="navbar"
+    >
+      <div>Logo O'Rent</div>
+      <SearchBar />
+      <Login />
+    </Box>
+  );
+};
 export default NavBar;
