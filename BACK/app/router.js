b46d7bb039ee3256f50d4af5a35a9509b0918c4d
AlexandreDemartin <alx.demartin@gmail.com>
2022-01-23 21:02:48 +0100

    setup Joi

--- BACK/app/router.js
+++ BACK/app/router.js
@@ -7,6 +7,13 @@ const checkJWT = require('./middlewares/checkJWT')
 
 const authController = require('./controllers/authController');
 
+
+/**
+ * GET /
+ * @summary Welcome Route
+ * @tags O'Rent
+ * @returns {string} 200 - `Bienvenue sur l'API Orent`
+ */
 router.get('/', (req, res) => {
     res.send(`Bienvenue sur l'API Orent`);
 })
@@ -14,9 +21,26 @@ router.get('/', (req, res) => {
 
 // ROUTES AUTH
 
-router.post('/login', authController.login);
+/**
+* @typedef UserPost
+* @property {string} username
+* @property {string} firstname
+* @property {string} lastname
+* @property {string} email
+* @property {string} password
+*/
 
+/**
+ * POST /register
+ * @summary Adds a new user in database
+ * @tags Auth
+ * @param {UserPost} request.body.required User object to add in database
+ * @returns {User} 201 - The newly created Users (view)
+ * @return {string} 409 - An error message because the user already exists
+ */
 router.post('/register', authController.register);
+
+router.post('/login', authController.login);
 
 router.post('/refresh-token', checkRefreshJWT, authController.refreshToken)
 
