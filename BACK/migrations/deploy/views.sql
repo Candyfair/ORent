86533897e6598f7fcdb88de09c45f9ff14beb493
AlexandreDemartin <alx.demartin@gmail.com>
2022-01-23 14:26:08 +0100

    heroku setup

--- BACK/migrations/deploy/views.sql
+++ BACK/migrations/deploy/views.sql
@@ -2,6 +2,7 @@
 
 BEGIN;
 
+
 CREATE VIEW "users_bookings" AS
 SELECT
     "user".id,
@@ -9,7 +10,26 @@ SELECT
 FROM "user"
     JOIN "booking" ON "booking".user_id="user".id
 GROUP BY "user".id
-ORDER BY "user".id
+ORDER BY "user".id;
+
+CREATE VIEW "properties_has_images" AS
+SELECT
+    "property".id,
+    ARRAY_AGG("property_image".url) images
+FROM "property"
+    JOIN "property_image" ON "property_image".property_id="property".id
+GROUP BY "property".id
+ORDER BY "property".id;
+
+CREATE VIEW "properties_offers_vacancies" AS
+SELECT
+    "property".id,
+    ARRAY_AGG("vacancy".id) vacancies
+FROM "property"
+    JOIN "vacancy" ON "vacancy".property_id="property".id
+GROUP BY "property".id
+ORDER BY "property".id;
+
 
 CREATE VIEW "users" AS
 SELECT DISTINCT
@@ -21,8 +41,36 @@ SELECT DISTINCT
     "user".created_at,
     "user".updated_at,
     "users_bookings".bookings AS bookings
-FROM "users"
-    JOIN "users_bookings" ON "users_bookings".id = "user".id
+FROM "user"
+   LEFT JOIN "users_bookings" ON "users_bookings".id = "user".id
+ORDER BY "user".id ASC;
 
+CREATE VIEW "properties" AS
+SELECT DISTINCT
+    "property".id,
+    "property".name,
+    "property".description,
+    "property".capacity,
+    "property".bedrooms_count,
+    "property".beds_count,
+    "property".bathrooms_count,
+    "property".type,
+    "property". street_number,
+    "property".street_name,
+    "property".zip_code,
+    "property".city,
+    "property".country,
+    "property".latitude,
+    "property".longitude,
+    "user".username AS host,
+    "properties_has_images".images AS images,
+    "properties_offers_vacancies".vacancies AS vacancies,
+    "property".created_at,
+    "property".updated_at
+FROM "property"
+    JOIN "user" ON "user".id = "property".user_id
+    JOIN "properties_has_images" ON "properties_has_images".id="property".id
+    JOIN "properties_offers_vacancies" ON "properties_offers_vacancies".id="property".id
+ORDER BY "property".created_at DESC;
 
 COMMIT;
--- BACK/migrations/revert/views.sql
+++ BACK/migrations/revert/views.sql
@@ -2,6 +2,10 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
+DROP VIEW "properties";
+DROP VIEW "users";
+DROP VIEW "properties_offers_vacancies";
+DROP VIEW "properties_has_images";
+DROP VIEW "users_bookings";
 
 COMMIT;
