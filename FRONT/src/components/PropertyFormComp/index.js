b7af88c2402ff951553f55a9903031fef34b4975
Candy Fair <candice.bastien@gmail.com>
2022-01-26 17:31:32 +0100

    Add icon to upload photo field

--- FRONT/src/components/PropertyFormComp/index.js
+++ FRONT/src/components/PropertyFormComp/index.js
@@ -33,135 +33,144 @@ const PropertyFormComp = () => {
 
   return (
     <form>
-      <FormControl>
-        <Stack
-          className={classes.propertyContainer}
-          spacing={3}
-        >
-          {/* Property name & photo upload */}
-          <Stack
-            spacing={1}
-          >
-            <TextField
-              required
-              id="name"
-              label="Name of your property"
-            />
 
-            <Input
-              type="file"
-              id="upload"
-              label="Upload photos"
-              endAdornment={(
+      <Stack
+        className={classes.propertyContainer}
+        spacing={3}
+      >
+        {/* Property name & photo upload */}
+        <Stack
+          spacing={1}
+        >
+          <TextField
+            required
+            id="name"
+            label="Name of your property"
+            variant="outlined"
+          />
+
+          <TextField
+            accept="image/*"
+            id="upload"
+            label="Upload photos"
+            variant="outlined"
+            multiple
+            InputProps={{
+              endAdornment: (
                 <InputAdornment position="end">
                   <PhotoIcon fontSize="small" />
                 </InputAdornment>
-              )}
+              ),
+            }}
+          />
+
+        </Stack>
+
+        {/* Address */}
+        <Stack
+          spacing={1}
+        >
+          <Typography variant="h5">Address</Typography>
+
+          <TextField
+            id="number"
+            label="Number"
+          />
+          <TextField
+            id="street"
+            label="Street"
+          />
+          <TextField
+            id="zipcode"
+            label="Zip code"
+          />
+          <TextField
+            required
+            id="city"
+            label="City"
+          />
+          <TextField
+            required
+            id="country"
+            label="Country"
+          />
+        </Stack>
+
+        {/* Features */}
+        <Stack
+          spacing={1}
+        >
+          <Typography variant="h5">Features</Typography>
+          <Stack
+            flexDirection="row"
+            className={classes.features}
+            gap={2}
+          >
+            <TextField
+              required
+              id="capacity"
+              label="Capacity"
               variant="outlined"
-            />
-          </Stack>
-
-          {/* Address */}
-          <Stack
-            spacing={1}
-          >
-            <Typography variant="h5">Address</Typography>
-
-            <TextField
-              id="number"
-              label="Number"
+              className={classes.featuresItem}
             />
             <TextField
-              id="street"
-              label="Street"
+              id="bedrooms"
+              label="Bedrooms"
+              className={classes.featuresItem}
             />
             <TextField
-              id="zipcode"
-              label="Zip code"
+              id="beds"
+              label="Beds"
+              className={classes.featuresItem}
             />
             <TextField
-              required
-              id="city"
-              label="City"
+              id="bathrooms"
+              label="Bathrooms"
+              className={classes.featuresItem}
             />
-            <TextField
-              required
-              id="country"
-              label="Country"
-            />
-          </Stack>
-
-          {/* Features */}
-          <Stack
-            spacing={1}
-          >
-            <Typography variant="h5">Features</Typography>
-            <Stack
-              flexDirection="row"
-              className={classes.features}
-              gap={2}
-            >
-              <TextField
-                required
-                id="capacity"
-                label="Capacity"
-                className={classes.featuresItem}
-              />
-              <TextField
-                id="bedrooms"
-                label="Bedrooms"
-                className={classes.featuresItem}
-              />
-              <TextField
-                id="beds"
-                label="Beds"
-                className={classes.featuresItem}
-              />
-              <TextField
-                id="bathrooms"
-                label="Bathrooms"
-                className={classes.featuresItem}
-              />
-            </Stack>
-          </Stack>
-
-          {/* Description */}
-          <Stack
-            spacing={1}
-          >
-            <Typography variant="h5">Description *</Typography>
-            <TextField
-              id="description"
-              label="Enter a description of your property"
-              multiline
-              minRows={4}
-            />
-          </Stack>
-
-          {/* Price */}
-          <Stack
-            spacing={1}
-          >
-            <Typography variant="h5">Price</Typography>
-            <TextField
-              required
-              id="price"
-              label="Price"
-            />
-          </Stack>
-          <Stack
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Button
-              variant="contained"
-              className={classes.btsave}
-            >
-              Save & Publish
-            </Button>
           </Stack>
         </Stack>
-      </FormControl>
+
+        {/* Description */}
+        <Stack
+          spacing={1}
+        >
+          <Typography variant="h5">Description *</Typography>
+          <TextField
+            id="description"
+            label="Enter a description of your property"
+            multiline
+            minRows={4}
+          />
+        </Stack>
+
+        {/* Price */}
+        <Stack
+          spacing={1}
+        >
+          <Typography variant="h5">Price</Typography>
+          <TextField
+            required
+            id="price"
+            label="Price"
+            InputProps={{
+              startAdornment: <InputAdornment position="start">€</InputAdornment>,
+            }}
+          />
+        </Stack>
+        <Stack
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Button
+            variant="contained"
+            className={classes.btsave}
+          >
+            Save & Publish
+          </Button>
+        </Stack>
+      </Stack>
+
     </form>
   );
 };
