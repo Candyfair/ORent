4ba8016c0979d87c9cfa6e530b23cf0411e25e65
Candy Fair <candice.bastien@gmail.com>
2022-01-31 16:16:47 +0100

    Add styling to PropertyDetail page

--- FRONT/src/components/PropertyDetailsComp/Description/index.js
+++ FRONT/src/components/PropertyDetailsComp/Description/index.js
@@ -1,12 +1,13 @@
 /* eslint-disable linebreak-style */
 // IMPORTS
 import { makeStyles } from '@mui/styles';
-import { Stack, Typography } from '@mui/material/';
+import { Stack, Typography, Divider } from '@mui/material/';
 
 // MUI STYLES
 const useStyles = makeStyles((theme) => ({
   description: {
     width: '70%',
+    paddingRight: theme.spacing(5),
   },
 }));
 
@@ -25,6 +26,9 @@ const Description = () => {
       <Typography variant="subtitle1">
         [Nb Guests] guests - [Nb bedrooms] bedrooms - [Nb beds] beds - [Nb bathrooms] bathrooms
       </Typography>
+
+      <Divider />
+
       <Typography variant="body1">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
       </Typography>
