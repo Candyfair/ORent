a450014c98fc3945600f6f8157a0c4aa81718841
Candy Fair <candice.bastien@gmail.com>
2022-01-31 13:12:38 +0100

    Add styling to vacancies list

--- FRONT/src/components/PropertyDetailsComp/index.js
+++ FRONT/src/components/PropertyDetailsComp/index.js
@@ -1,17 +1,41 @@
 /* eslint-disable linebreak-style */
 // === Imports
+import { makeStyles } from '@mui/styles';
+import { Stack, Typography } from '@mui/material/';
+
 import Pictures from './Pictures';
 import Description from './Description';
 import VacanciesList from './VacanciesList';
 
-const PropertyDetailsComp = () => (
-  <div>
-    <Pictures />
-    <div className="PropertyDetailsContainer">
-      <Description />
-      <VacanciesList />
-    </div>
-  </div>
-);
+// MUI STYLES
+const useStyles = makeStyles((theme) => ({
+
+}));
+
+// COMPONENT
+const PropertyDetailsComp = () => {
+  const classes = useStyles();
+
+  return (
+    <Stack
+      flexDirection="column"
+      spacing={5}
+    >
+
+      {/* Photos de la propriété */}
+      <Pictures />
+
+      <Stack
+        className="PropertyDetailsContainer"
+        flexDirection="row"
+        justifyContent="space-between"
+      >
+        <Description />
+        <VacanciesList />
+      </Stack>
+
+    </Stack>
+  );
+};
 
 export default PropertyDetailsComp;
