8360ffa18c84dedf3a46a66f47631121d775ede8
Candy Fair <candice.bastien@gmail.com>
2022-02-01 13:03:50 +0100

    Rearrange PropertyDetails files structure

--- FRONT/src/pages/PropertyDetails/index.js
+++ FRONT/src/pages/PropertyDetails/index.js
@@ -5,13 +5,16 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router';
 import { useEffect } from 'react';
 
-import PropertyDetailsComp from '../../components/PropertyDetailsComp';
-import { fetchProperty } from '../../redux/actions/propertiesFetch';
+import Pictures from '../../components/PropertyDetailsComp/Pictures';
+import Description from '../../components/PropertyDetailsComp/Description';
+import VacanciesList from '../../components/PropertyDetailsComp/VacanciesList';
 import Loader from '../../components/Loader';
+
+import { fetchProperty } from '../../redux/actions/propertiesFetch';
 
 // MUI STYLES
 const useStyles = makeStyles((theme) => ({
-  propertyDetail: {
+  propertyDetailClass: {
     padding: theme.spacing(3),
     maxWidth: 1165,
   },
@@ -24,8 +27,8 @@ const PropertyDetails = () => {
   const { id } = useParams();
 
   const { loading } = useSelector((state) => state.displayOptions);
-
   const { propertyDetails } = useSelector((state) => state.propertyCurrent);
+
   useEffect(() => {
     dispatch(fetchProperty(id));
   }, []);
@@ -38,7 +41,7 @@ const PropertyDetails = () => {
       spacing={5}
       justifyContent="left"
       alignItems="left"
-      className={classes.propertyDetail}
+      className={classes.propertyDetailClass}
     >
       <Typography
         variant="h4"
@@ -47,7 +50,33 @@ const PropertyDetails = () => {
         {propertyDetails.name}
       </Typography>
 
-      <PropertyDetailsComp details={propertyDetails} />
+      <Stack
+        flexDirection="column"
+        spacing={5}
+      >
+
+        {/* Photos de la propriété */}
+        <Pictures />
+
+        <Stack
+          flexDirection="row"
+          justifyContent="space-between"
+        >
+          <Description
+            type={propertyDetails.type}
+            city={propertyDetails.city}
+            country={propertyDetails.country}
+            host={propertyDetails.host}
+            capacity={propertyDetails.capacity}
+            bedroomscount={propertyDetails.bedroomscount}
+            bedscount={propertyDetails.bedscount}
+            bathroomscount={propertyDetails.bathroomscount}
+            description={propertyDetails.description}
+          />
+          <VacanciesList vacancies={propertyDetails} />
+        </Stack>
+
+      </Stack>
     </Stack>
   );
 };
