83785180b6315f896b85dc3c32ef2257f69fca47
Candy Fair <candice.bastien@gmail.com>
2022-02-02 11:43:05 +0100

    Display gallery for desktop

--- FRONT/src/components/PropertyDetailsComp/Pictures/index.js
+++ FRONT/src/components/PropertyDetailsComp/Pictures/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable linebreak-style */
 // IMPORTS
+import { makeStyles } from '@mui/styles';
 import {
   Stack, ImageList, ImageListItem,
 } from '@mui/material/';
@@ -16,23 +17,53 @@ function srcset(image, size, rows = 1, cols = 1) {
   };
 }
 
+// MUI STYLES
+const useStyles = makeStyles(() => ({
+  photosDesktop: {
+    
+  },
+  photosMobile: {
+
+  },
+}));
+
+// COMPONENT
 const Pictures = ({ images }) => {
-  const itemData = images;
+  const classes = useStyles();
+
+  // New photos arrays generated
+  const mainImage = images.find((image) => image[0]);
+  const newArray = [...images];
+  const otherImages = newArray.slice(1, newArray.length);
 
   return (
     <Stack
       flexDirection="row"
-      gap={1}
+      gap={0.5}
     >
+      {/* Première image desktop */}
+      <Stack className={classes.photosDesktop}>
+        <ImageList sx={{ width: 560, height: 504 }} cols={1}>
+          <ImageListItem>
+            <img
+              src={`${mainImage}?w=560&h=500&fit=crop&auto=format`}
+              alt="My property"
+            />
+
+          </ImageListItem>
+        </ImageList>
+      </Stack>
+
+      {/* Autres images desktop */}
       <Stack>
 
         <ImageList
           variant="quilted"
-          col={4}
+          col={2}
           rowHeight={250}
         >
           {
-            itemData.map((item) => (
+            otherImages.map((item) => (
               <ImageListItem key={item} cols={1} rows={1}>
                 <img
                   {...srcset(item, 121, item, item)}
