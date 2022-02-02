13db115d2c84f0ba13c0785819ddacf467e5f05a
Candy Fair <candice.bastien@gmail.com>
2022-02-02 15:43:26 +0100

    Add arrows to mobile slider

--- FRONT/src/components/PropertyDetailsComp/Pictures/index.js
+++ FRONT/src/components/PropertyDetailsComp/Pictures/index.js
@@ -1,11 +1,17 @@
 /* eslint-disable linebreak-style */
 // IMPORTS
+import { useState } from 'react';
 import { makeStyles } from '@mui/styles';
 import {
-  Stack, ImageList, ImageListItem, Box,
+  Stack, ImageList, ImageListItem, Box, MobileStepper,
+  IconButton,
 } from '@mui/material/';
+import { useTheme } from '@mui/material/styles';
+import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
+import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 
 import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
 
 import PropTypes from 'prop-types';
 
@@ -47,31 +53,92 @@ const Pictures = ({ images }) => {
   const newArray = [...images];
   const otherImages = newArray.slice(1, newArray.length);
 
+  // Slider for mobile
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = useState(0);
+  const maxSteps = images.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
   return (
     <Stack
       flexDirection="row"
       gap={0.5}
       className={classes.desktopPadding}
     >
+      {/* Mobile view */}
       <Stack className={classes.photosMobile}>
-        <SwipeableViews>
-          {images.map((item) => (
-            <div key={item}>
-              <Box
-                component="img"
-                sx={{
-                  minHeight: 255,
-                  display: 'block',
-                  maxWidth: '100%',
-                  overflow: 'hidden',
-                }}
-                src={item}
-                alt="Property"
-              />
-            </div>
 
+        <SwipeableViews
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          index={activeStep}
+          onChangeIndex={handleStepChange}
+          enableMouseEvents
+        >
+          {images.map((step, index) => (
+            <div key={step}>
+              {Math.abs(activeStep - index) <= 2 ? (
+                <Box
+                  component="img"
+                  sx={{
+                    minHeight: 255,
+                    display: 'block',
+                    maxWidth: 400,
+                    overflow: 'hidden',
+                    width: '100%',
+                  }}
+                  src={step}
+                  alt={step}
+                />
+              ) : null}
+            </div>
           ))}
         </SwipeableViews>
+
+        <Stack
+          flexDirection="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <MobileStepper
+            steps={maxSteps}
+            variant="dots"
+            position="static"
+            activeStep={activeStep}
+            sx={{ maxWidth: 400, flexGrow: 1 }}
+            nextButton={(
+              <IconButton
+                color="primary"
+                aria-label="upload picture"
+                component="span"
+                onClick={handleNext}
+                disabled={activeStep === maxSteps - 1}
+              >
+                <ArrowForwardIosRoundedIcon />
+              </IconButton>
+            )}
+            backButton={(
+              <IconButton
+                color="primary"
+                aria-label="upload picture"
+                component="span"
+                onClick={handleBack}
+                disabled={activeStep === 0}
+              >
+                <ArrowBackIosNewRoundedIcon />
+              </IconButton>
+            )}
+          />
+        </Stack>
+
       </Stack>
 
       {/* Première image desktop */}
