5d26870be02c7ed0ab86ca502ce5814d5fe83abf
AlexandreDemartin <alx.demartin@gmail.com>
2022-02-04 16:01:54 +0100

    bouton pour ajouter un vacancy fonctionnel

--- FRONT/src/middleware/vacancyMiddleware.js
+++ FRONT/src/middleware/vacancyMiddleware.js
@@ -1,4 +1,5 @@
-import { ADD_VACANCY, FETCH_PROPERTY_VACANCIES, setPropertyVacancies } from '../redux/actions/vacancy';
+import { ADD_VACANCY, FETCH_PROPERTY_VACANCIES, setNewVacancy, setPropertyVacancies } from '../redux/actions/vacancy';
+import { setSnackbar } from '../redux/actions/displayOptions';
 
 import api from './api';
 
@@ -14,18 +15,22 @@ const vacancyMiddleware = (store) => (next) => (action) => {
         .then(
           (response) => {
             console.log('Réussite sur la route POST /vacancies : ', response);
+            store.dispatch(setNewVacancy(response.data));
+            store.dispatch(setSnackbar(true, 'The vacancy was successfull added !','success'))
           },
         ).catch((error) => {
           console.log('error sur la route POST /vacancies : ', error);
+          store.dispatch(setSnackbar(true, 'An error occured !','error'))
+
         });
       next(action);
       break;
     }
     case FETCH_PROPERTY_VACANCIES: {
-      api.get(`/vacancies/property/${action.id}`)
+      api.get(`/vacancies/property/${action.propertyId}`)
         .then(
           (response) => {
-            console.log(`Réussite sur la route GET /vacancies/property/${action.id} :`, response);
+            console.log(`Réussite sur la route GET /vacancies/property/${action.propertyId} :`, response);
             store.dispatch(setPropertyVacancies(response.data));
           },
         ).catch((error) => {
