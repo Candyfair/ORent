09264961a2c2804326a4fc29e5c78775ae9aa2c0
Candy Fair <candice.bastien@gmail.com>
2022-02-09 15:39:22 +0100

    Add about page

--- FRONT/src/pages/About/index.js
+++ FRONT/src/pages/About/index.js
@@ -1,10 +1,139 @@
-const About = () => (
-  <div id="container">
-    <h1>ABOUT US :</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe in suscipit deserunt laboriosam, provident ipsum consectetur vel molestias. Eligendi beatae cupiditate veritatis reprehenderit obcaecati eaque reiciendis sint id, praesentium in.
-    </p>
-  </div>
-);
+/* eslint-disable max-len */
+// IMPORTS
+import { makeStyles } from '@mui/styles';
+import {
+  Stack, Typography, Card, CardMedia, CardContent,
+} from '@mui/material/';
+import img1 from 'src/assets/images/house1.jpg';
+import img2 from 'src/assets/images/house2.jpg';
+import img3 from 'src/assets/images/house3.jpg';
+
+// MUI STYLES
+const useStyles = makeStyles((theme) => ({
+  content: {
+    margin: '0 auto',
+    paddingTop: theme.spacing(2),
+    width: '45%',
+    minWidth: 800,
+    [theme.breakpoints.down('md')]: {
+      width: '80%',
+      minWidth: '80%',
+      paddingTop: theme.spacing(4),
+    },
+  },
+  bold: {
+    fontWeight: 600,
+  },
+  cardStyle: {
+    maxWidth: 250,
+    [theme.breakpoints.down('md')]: {
+      width: '100%',
+    },
+  },
+  image: {
+    width: 250,
+    height: 250,
+  },
+  team: {
+    [theme.breakpoints.down('md')]: {
+      flexDirection: 'column',
+    },
+  },
+}));
+
+// COMPONENT
+const About = () => {
+  const classes = useStyles();
+
+  return (
+    <Stack
+      className={classes.content}
+      spacing={2}
+    >
+      <Typography variant="h4">
+        About us
+      </Typography>
+
+      {/* Text */}
+      <Typography variant="body1">
+        <span className={classes.bold}>O'Rent</span> aims to connect homeowners who want to let their properties with private individuals who are looking for authentic places they can rent while travelling.
+      </Typography>
+      <Typography>
+        Please note that this website is for demo purpose only, and it's not currently possible to rent properties.
+      </Typography>
+      <Typography variant="body1">
+        This site has been coded by a team of 3 full-stack developers as an end-of-study project:
+      </Typography>
+
+      {/* Avatars */}
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        className={classes.team}
+      >
+
+        {/* Alexandre */}
+        <Card className={classes.cardStyle}>
+          <CardMedia>
+            <img src={img1} alt="House" className={classes.image} />
+          </CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Alexandre Demartin
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              Git Master
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              Technical Referent (Back)
+            </Typography>
+          </CardContent>
+        </Card>
+
+        {/* Candice */}
+        <Card className={classes.cardStyle}>
+          <CardMedia>
+            <img src={img3} alt="Cottage" className={classes.image} />
+          </CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Candice Fairand
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              Product Owner
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              Scrum Master
+            </Typography>
+          </CardContent>
+        </Card>
+
+        {/* Augustin */}
+        <Card className={classes.cardStyle}>
+          <CardMedia>
+            <img src={img2} alt="Cottage" className={classes.image} />
+          </CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Augustin Mbarga
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              Lead Developer
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              Technical Referent (Front)
+            </Typography>
+          </CardContent>
+        </Card>
+      </Stack>
+
+      <Typography variant="caption">
+        Resources: properties pictures by Unsplash and vector illustrations by Vecteezy
+      </Typography>
+
+    </Stack>
+  );
+};
 
 export default About;
--- FRONT/src/pages/PrivacyPolicy/index.js
+++ FRONT/src/pages/PrivacyPolicy/index.js
@@ -1,19 +1,62 @@
-const PrivacyPolicy = () => (
-  <div id="container">
-    <h1>Privacy policy</h1>
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit quas, accusantium officiis culpa ullam tenetur quisquam excepturi harum dolorem tempore fugit asperiores ratione similique possimus alias architecto obcaecati ea veritatis!
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, fuga quia aperiam reprehenderit excepturi culpa consequuntur debitis accusamus suscipit veritatis laudantium asperiores perspiciatis doloribus animi? Reiciendis libero quod veniam saepe.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque minus quisquam voluptatem dolores rem dolorem cupiditate maiores esse, explicabo ipsum, doloribus error non modi commodi voluptates quibusdam molestias, ipsa dolore.
-    </p>
-    <p>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam iure, expedita aut corrupti libero quam corporis soluta voluptatibus, recusandae quisquam ut omnis alias accusamus, impedit vitae earum totam reiciendis nulla!
-    </p>
-  </div>
-);
+// IMPORTS
+import { makeStyles } from '@mui/styles';
+import { Stack, Typography } from '@mui/material/';
+
+// MUI STYLES
+const useStyles = makeStyles((theme) => ({
+  content: {
+    margin: '0 auto',
+    paddingTop: theme.spacing(2),
+    width: '60%',
+    [theme.breakpoints.down('md')]: {
+      width: '80%',
+      paddingTop: theme.spacing(4),
+    },
+  },
+
+}));
+
+// COMPONENT
+const PrivacyPolicy = () => {
+  const classes = useStyles();
+
+  return (
+    <Stack
+      className={classes.content}
+      spacing={2}
+    >
+      <Typography variant="h4">
+        Privacy Policy
+      </Typography>
+      <Typography variant="body">
+        Lorem ipsum dolor sit amet. Eum fugit repellendus aut magnam accusantium ad consequuntur ipsum est porro dolores. Eos deleniti harum ea fugit totam sit nostrum perferendis ad recusandae similique. Eos ullam repellendus non debitis rerum sit cumque eius ut dignissimos consequatur 33 minima natus vel vero totam cum recusandae doloribus. Ut voluptas nihil voluptatem sint a internos molestiae qui voluptatem quas et quia corrupti voluptas illo.
+      </Typography>
+
+      <Typography>
+        Sed delectus delectus qui doloribus adipisci id aspernatur reprehenderit. Hic exercitationem animi qui quod architecto non inventore dignissimos ut dolor dolor id impedit amet. Aut molestiae perspiciatis sed similique sint non minima nesciunt.
+      </Typography>
+      <Typography>
+
+        Eos inventore odio sed rerum sapiente in iure atque et repellat Quis et recusandae placeat! Est veritatis expedita et galisum quia est nobis quibusdam et facere cupiditate et incidunt facilis quo minus corporis ut aperiam eaque!
+      </Typography>
+      <Typography>
+
+        Eum asperiores error rem placeat voluptas et quis aspernatur qui eaque velit est ullam nisi sed eaque vitae. Et minima possimus et dolor exercitationem eos animi sequi.
+      </Typography>
+      <Typography>
+
+        Sit repellendus aut doloremque tempora rem accusantium autem id asperiores repudiandae. Qui minus aliquid et commodi voluptatem ut doloremque cumque et ullam magnam quo ducimus consequuntur et necessitatibus enim!
+      </Typography>
+      <Typography>
+
+        Ut reiciendis temporibus ut ratione voluptate ut quasi quasi et quibusdam perspiciatis ad velit quia eum possimus voluptatibus. Et quasi corrupti quo ipsa sunt est aspernatur nobis autem nulla est voluptatibus repellendus.
+      </Typography>
+      <Typography>
+
+        Rem dicta suscipit ut quidem molestias eos voluptatum aspernatur ad odio cumque non unde voluptas. Aut consequuntur eius a ullam eveniet qui dolorem minima quo minus voluptates quo maxime magnam sit laborum voluptate.
+      </Typography>
+    </Stack>
+  );
+};
 
 export default PrivacyPolicy;
